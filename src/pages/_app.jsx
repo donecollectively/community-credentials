15b3418ad1eee92d94582fac99c3cca5e4e848d7
Randall <randall@inator.biz>
2023-11-15 10:55:29 -0800

    pretty-print source

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,73 +1,74 @@
-import head from "next/head.js";  const Head = head.default;
-import { slugifyWithCounter } from '@sindresorhus/slugify'
+import head from "next/head.js";
+const Head = head.default;
+import { slugifyWithCounter } from "@sindresorhus/slugify";
 
-import { Layout } from '@/components/Layout'
-import Script from 'next/script'
+import { Layout } from "@/components/Layout";
+import Script from "next/script";
 
-import 'focus-visible'
-import '@/styles/tailwind.css'
+import "focus-visible";
+import "@/styles/tailwind.css";
 
 function getNodeText(node) {
-  let text = ''
-  for (let child of node.children ?? []) {
-    if (typeof child === 'string') {
-      text += child
+    let text = "";
+    for (let child of node.children ?? []) {
+        if (typeof child === "string") {
+            text += child;
+        }
+        text += getNodeText(child);
     }
-    text += getNodeText(child)
-  }
-  return text
+    return text;
 }
 
 function collectHeadings(nodes, slugify = slugifyWithCounter()) {
-  let sections = []
+    let sections = [];
 
-  for (let node of nodes) {
-    if (node.name === 'h2' || node.name === 'h3') {
-      let title = getNodeText(node)
-      if (title) {
-        let id = slugify(title)
-        node.attributes.id = id
-        if (node.name === 'h3') {
-          if (!sections[sections.length - 1]) {
-            throw new Error(
-              'Cannot add `h3` to table of contents without a preceding `h2`'
-            )
-          }
-          sections[sections.length - 1].children.push({
-            ...node.attributes,
-            title,
-          })
-        } else {
-          sections.push({ ...node.attributes, title, children: [] })
+    for (let node of nodes) {
+        if (node.name === "h2" || node.name === "h3") {
+            let title = getNodeText(node);
+            if (title) {
+                let id = slugify(title);
+                node.attributes.id = id;
+                if (node.name === "h3") {
+                    if (!sections[sections.length - 1]) {
+                        throw new Error(
+                            "Cannot add `h3` to table of contents without a preceding `h2`"
+                        );
+                    }
+                    sections[sections.length - 1].children.push({
+                        ...node.attributes,
+                        title,
+                    });
+                } else {
+                    sections.push({ ...node.attributes, title, children: [] });
+                }
+            }
         }
-      }
+
+        sections.push(...collectHeadings(node.children ?? [], slugify));
     }
 
-    sections.push(...collectHeadings(node.children ?? [], slugify))
-  }
-
-  return sections
+    return sections;
 }
 
 export default function App({ Component, pageProps }) {
-    let {notProse=false} = Component
-    let title = pageProps.markdoc?.frontmatter.title
+    let { notProse = false } = Component;
+    let title = pageProps.markdoc?.frontmatter.title;
 
-  let pageTitle =
-    pageProps.markdoc?.frontmatter.pageTitle ||
-    `${pageProps.markdoc?.frontmatter.title}`
+    let pageTitle =
+        pageProps.markdoc?.frontmatter.pageTitle ||
+        `${pageProps.markdoc?.frontmatter.title}`;
 
-  let description = pageProps.markdoc?.frontmatter.description
+    let description = pageProps.markdoc?.frontmatter.description;
 
-  let tableOfContents = pageProps.markdoc?.content
-    ? collectHeadings(pageProps.markdoc.content)
-    : []
+    let tableOfContents = pageProps.markdoc?.content
+        ? collectHeadings(pageProps.markdoc.content)
+        : [];
 
-const GA_ID = "XXXX!!!customize";
-  return (
-    <>
-      <Head>
-        {/* <Script async src={`https://www.googletagmanager.com/gtag/js?id={GA_ID}`}></Script>
+    const GA_ID = "XXXX!!!customize";
+    return (
+        <>
+            <Head>
+                {/* <Script async src={`https://www.googletagmanager.com/gtag/js?id={GA_ID}`}></Script>
         <Script type='text/javascript' children={`
         if (typeof window !== 'undefined') {
             window.dataLayer = window.dataLayer || [];
@@ -77,13 +78,14 @@ const GA_ID = "XXXX!!!customize";
             gtag('config', '${GA_ID}');
         } 
             `} /> */}
-    <title>{pageTitle}</title>
-        {description && <meta name="description" content={description} />}
-
-      </Head>
-      <Layout {...{title, tableOfContents, notProse}}>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  )
+                <title>{pageTitle}</title>
+                {description && (
+                    <meta name="description" content={description} />
+                )}
+            </Head>
+            <Layout {...{ title, tableOfContents, notProse }}>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
