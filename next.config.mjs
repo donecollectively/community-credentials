2f936d2b394c7f051f8b459e2b8918b704bf2590
Randall <randall@inator.biz>
2023-11-23 12:37:54 -0800

    try again with standalone

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: "export",
+    output: "standalone",
     basePath: '/community-credentials',
     trailingSlash: true,
     pageExtensions: ["js", "jsx", "tsx", "md"],
