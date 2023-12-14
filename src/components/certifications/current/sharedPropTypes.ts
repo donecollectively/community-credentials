8aebe03ad7b1ecd5b4bf6deb8df0fcc194470d5f
Randall <randall@inator.biz>
2023-12-14 08:58:05 -0800

    v0.6-alpha2: fix an import

--- src/components/certifications/current/sharedPropTypes.ts
+++ src/components/certifications/current/sharedPropTypes.ts
@@ -1,5 +1,5 @@
 import { TxInput, WalletHelper, helios } from "@donecollectively/stellar-contracts";
-import { CCRegistry } from "../../../contracts/current/CCRegistry.js"
+import { CCRegistryV06 } from "../../../contracts/current/CCRegistry.js"
 
 const { BlockfrostV0, Cip30Wallet, TxChain } = helios;
 type hBlockfrost = typeof BlockfrostV0.prototype;
@@ -7,7 +7,7 @@ type hTxChain = typeof TxChain.prototype;
 type hWallet = typeof Cip30Wallet.prototype;
 
 export type credRegistryProps = {
-    credsRegistry: CCRegistry;
+    credsRegistry: CCRegistryV06;
     wallet? : hWallet;
     walletUtxos? : TxInput[],
     walletHelper? : WalletHelper
